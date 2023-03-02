acc9a04424bf00382132c976e18812ab8db3bc71
Rafał Gwoździk <76224256+rafko12@users.noreply.github.com>
2023-03-02 12:33:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -123,14 +123,6 @@ start.addEventListener('click', () => {
 	startBtn()
 })
 
-if (start.classList.contains('hidden') !== true) {
-	document.addEventListener('keyup', () => {
-		start.classList.add('hidden')
-		timer.classList.remove('hidden')
-		startBtn()
-	})
-}
-
 //funkcja po nacisnieciu przycisku START
 const startBtn = () => {
 	timer.innerHTML = '3'
